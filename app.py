85aff5feb7e079c2bc300fe1ae419e8e23efac11
Wojciech Sylwester <woj.sylwester@gmail.com>
2023-02-20 10:26:29 +0100

    build: streamlit app

--- app.py
+++ app.py
@@ -1,21 +1,45 @@
-from flask import Flask, render_template, request
+import streamlit as st
 import recommendation_system as r
 
-app = Flask(__name__)
+header = st.container()
 
-@app.route('/')
-def man():
-    return render_template('home.html')
+st.markdown(
+    '''
+    <style>
+    .main {
+    background-color: #F5F5F5;
+    }
+    </style>
+    ''',
+    unsafe_allow_html=True
+)
 
-
-@app.route('/predict', methods=['POST'])
-def predict():
-
-    alph = 'abcdefghijklmnoprstuwxyz'
-    arr = [int(request.form[i]) for i in alph]
-
+@st.cache_data
+def get_data():
     df_train1505 = r.load_csv()
-    df_train1505 = r.add_user_input(arr, df_train1505)
+    return df_train1505
+
+with header:
+    st.title("nagloweeek")
+    st.header('robie to dlatego bo i to ma taką logikę')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    arr1 = [ (1 if (st.radio('prod'+str(i), ['nie posiadam', 'posiadam'], horizontal=True, index=0)) == 'posiadam' else 0) for i in range(1,13)]
+
+    st.text(arr1)
+
+with col2:
+    arr2 = [ (1 if (st.radio('prod'+str(i), ['nie posiadam', 'posiadam'], horizontal=True, index=0)) == 'posiadam' else 0) for i in range(13,25)]
+
+    st.text(arr2)
+
+click = st.button('pred')
+
+if click:
+    df_train1505 = get_data()
+    df_train1505 = r.add_user_input(arr1 + arr2, df_train1505)
     df_ui = r.df_useritem(df_train1505)
     cos_sim = r.cos_sim(df_ui)
     ui = r.useritem(0, df_ui, sim_matrix = cos_sim)
@@ -26,7 +50,5 @@ def predict():
 
     rec = r.recommendation(0, df_mb, hybrid_rec)
 
-    return render_template('home.html', recom = 'Top 7 Recommendations: {}'.format(rec))
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    st.text('Najbardziej rekomendowane produkty to:')
+    for ix, product in enumerate(rec): st.write(str(ix + 1), ". ", product)
