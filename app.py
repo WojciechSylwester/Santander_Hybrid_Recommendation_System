a1d80c11cd0afd8378b3e6c63f3054d7022e856b
Wojciech Sylwester <woj.sylwester@gmail.com>
2023-02-21 18:42:22 +0100

    feat: verify ownership of all products

--- app.py
+++ app.py
@@ -46,17 +46,26 @@ if click:
     
     with st.spinner('Due to the calculation of the machine learning model you have to wait a few seconds for the result.'):
 
-        df_train1505 = get_data()
-        df_train1505 = r.add_user_input(arr1 + arr2, df_train1505)
-        df_ui = r.df_useritem(df_train1505)
-        cos_sim = r.cos_sim(df_ui)
-        ui = r.useritem(0, df_ui, sim_matrix = cos_sim)
-        df_mb = r.df_mb(df_train1505)
+        if sum(arr1 + arr2) == len(product_list):
 
-        hybrid_rec = r.hybrid(0, df_p = df_train1505, df_u = df_ui, sim_matrix=cos_sim, df_m = df_mb, f1 = 0.5, f2 = 0.25, f3 = 0.25)
+            st.success('You already have all the products.')
+        
+        else:
 
-        rec = r.recommendation(0, df_mb, hybrid_rec)
+            df_train1505 = get_data()
+            df_train1505 = r.add_user_input(arr1 + arr2, df_train1505)
+            df_ui = r.df_useritem(df_train1505)
+            cos_sim = r.cos_sim(df_ui)
+            ui = r.useritem(0, df_ui, sim_matrix = cos_sim)
+            df_mb = r.df_mb(df_train1505)
+
+            hybrid_rec = r.hybrid(0, df_p = df_train1505, df_u = df_ui, sim_matrix=cos_sim, df_m = df_mb, f1 = 0.5, f2 = 0.25, f3 = 0.25)
+
+            rec = r.recommendation(0, df_mb, hybrid_rec)
+
+            st.write('Recommended products:')
+            
+            for ix, product in enumerate(rec):
+                st.write(str(ix + 1), ". ", product)
 
 
-    st.write('Recommended products:')
-    for ix, product in enumerate(rec): st.write(str(ix + 1), ". ", product)
